ba74090678cc883a4efc680a15e9672ac163099f
theaneryy00 <111473139+theaneryy00@users.noreply.github.com>
2022-09-05 09:54:39 +0800

    Create App.java

---
+++ Exam_1/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) {
+
+        // Character Class
+        Student me = new Student( "Nery", "Thea", 'J', 12/04/03, 2022102810, "nerytj@students.national-u.edu.ph", true );
+        me.sayMyStudentNumber();
+        me.sayMystudentEmailAddress();
+        me.sayMyamIAwesome();
+        
+
+ 
+    
+
+    }
+}
